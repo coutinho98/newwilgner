4ecf1b1867c5b674560d2be198c8a9957843760e
coutinho98 <mateuscouto98@outlook.com>
2026-01-31 12:36:41 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen w-full">
+      <h1 className="text-3xl font-bold">Hi Shadys :)</h1>
+    </div>
+  )
+}
+
+export default App
